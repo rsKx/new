e101f67913d92cf0b43bfed482a74db0ce65a6d0
rsKx <rodrigoskaneko@gmail.com>
2021-03-05 03:49:58 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats' component={HatsPage} />
     </div>
   );
 }
